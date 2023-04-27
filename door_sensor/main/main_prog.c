31f5936d018433dab22e281225a1f2ca0b5bfd61
Aya Kassem <91101614+ayak2002@users.noreply.github.com>
2023-04-26 20:35:57 -0400

    Update main_prog.c

--- door_sensor/main/main_prog.c
+++ door_sensor/main/main_prog.c
@@ -10,15 +10,15 @@ static const char *TAG = "door_sensor";
 void app_main(void){
 
     while(true){
-        gpio_set_direction(DOOR_SENSOR_PIN, GPIO_MODE_INPUT);
+        gpio_set_direction(DOOR_SENSOR_PIN, GPIO_MODE_INPUT); //set pin mode
 
 
-        if (gpio_get_level(DOOR_SENSOR_PIN) == 0){  
-            ESP_LOGI(TAG, "INTRUDER ALERT");    
+        if (gpio_get_level(DOOR_SENSOR_PIN) == 0){  //if low, the two parts of the switch have been separated, aka the door has been opened
+            ESP_LOGI(TAG, "INTRUDER ALERT");    //send an alert
         } 
         else{
             ESP_LOGI(TAG, "No intruder");
         }
-        vTaskDelay(100);
+        vTaskDelay(100); 
     }
 }
